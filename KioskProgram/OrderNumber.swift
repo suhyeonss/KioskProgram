e136b2bb8f9afeee029f49fefeb55ecc3df4e449
junyoung_hong <wnsdud0721@naver.com>
2023-07-25 14:23:48 +0900

    junyoung브랜치의 OrderNumber.swift파일 추가

---
+++ KioskProgram/OrderNumber.swift
@@ -0,0 +1,30 @@
+//
+//  OrderNumber.swift
+//  KioskProgram
+//
+//  Created by Junyoung_Hong on 2023/07/24.
+//
+
+import Foundation
+
+class OrderNumber {
+    var togo: Int = 0
+    var forhere: Int = 0
+    
+    func printOrderNumber (package: String) {
+        switch package {
+        case "1":
+            togo += 1
+            print("주문번호 : A-\(forhere)\n")
+        case "2":
+            forhere += 1
+            print("주문번호 : B-\(togo)\n")
+        case "q":
+            kioskPower = false
+            break
+        default:
+            print("다시 입력해주세요.")
+            
+        }
+    }
+}
