07ff223468d08afe5b48af746c3be5f883f0cc1a
jakkujakku <jakkujakku98@gmail.com>
2023-07-25 13:56:30 +0900

    결제 클래스 병합

---
+++ KioskProgram/Payment/Mobile.swift
@@ -0,0 +1,31 @@
+//
+//  Mobile.swift
+//  KioskProgram
+//
+//  Created by (^ㅗ^)7 iMac on 2023/07/24.
+//
+
+import Foundation
+
+// 모바일 상품권
+class Mobile: Payment {
+
+    // 결제방식 선택여부
+    override func choosePayment(input: String) {
+        switch input {
+        case "1":
+            print("모바일 상품권 결제")
+        case "2":
+            print("모바일 상품권 결제 취소")
+        default:
+            print("잘못된 번호 선택하셨습니다.")
+            break
+        }
+    }
+    
+    override func checkListUserPayment() {
+        print("[모바일 상품권 결제 페이지입니다.]")
+        let dic = ["1":"모바일 상품권 결제완료", "2":"모바일 상품권 결제 취소"]
+        dic.sorted { $0 < $1 }.forEach { print("\($0.key). \($0.value)")}
+    }
+}
---
+++ KioskProgram/Payment/Payment.swift
@@ -0,0 +1,52 @@
+//
+//  Payment.swift
+//  KioskProgram
+//
+//  Created by (^ㅗ^)7 iMac on 2023/07/24.
+//
+
+import Foundation
+
+// 전체 결제 클래스(부모 클래스)
+class Payment {
+    var paymentDic = ["1":"신용카드", "2":"현금", "3":"모바일", "0":"뒤로가기"]
+    
+    // 메뉴얼 표시(공통)
+    func showList() {
+        print("[아래 메뉴얼을 보시고 결제 방식을 골라 입력해주세요.]")
+        paymentDic.sorted { $0 < $1 }.forEach { print("\($0.key). \($0.value)")}
+    }
+    
+    // 결제선택(공통)
+    func selectedPayment(input: String) -> Int {
+        switch input {
+        case "1":
+            print("<신용카드를 선택하셨습니다.>")
+            return 1
+        case "2":
+            print("<현금을 선택하셨습니다.>")
+            return 2
+        case "3":
+            print("<모바일 상품권 선택하셨습니다.>")
+            return 3
+        case "0":
+            print("<뒤로가기>")
+            return 0
+        default:
+            print("<잘못된 번호를 선택하셨습니다.>")
+            return -1
+        }
+        
+    }
+    
+    // 결제방식 보여주는 함수
+    func checkListUserPayment() {
+        
+    }
+    
+    // 결제방식 여부 선택
+    func choosePayment(input: String) {
+        
+    }
+    
+}
