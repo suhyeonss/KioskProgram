dc8bdbb76872f68a45e92afedba31a464d4221ab
suzzang <hyeonnee98@gmail.com>
2023-07-26 11:21:52 +0900

    small change

--- KioskProgram/Basket/BasketFunctions.swift
+++ KioskProgram/Basket/BasketFunctions.swift
@@ -11,10 +11,10 @@ class BasketFunctions: Basket {
     
     override func showCurrentBasket() {
         if basketList.isEmpty {
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("장바구니가 비어있습니다.")
         } else {
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("-장바구니-")
             for i in (0..<basketList.count) {
                 if case let .name(name) = basketList[i] {
@@ -35,7 +35,7 @@ class BasketFunctions: Basket {
     
     //장바구니의 상품을 삭제하는 메소드
     override func removeProduct() {
-        print("========================================")
+        print(String(repeating: "=", count: 30))
         print("-장바구니-")
         for i in (0..<basketList.count) {
             if case let .name(name) = basketList[i] {
@@ -43,34 +43,34 @@ class BasketFunctions: Basket {
             }
         }
         
-        print("========================================")
+        print(String(repeating: "=", count: 30))
         print("삭제할 상품의 번호를 입력하세요. \n0번은 취소입니다.")
         let deleteNumber = Int(readLine() ?? "0")
         if deleteNumber!-1 >= basketList.count {
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("삭제할 상품의 번호가 존재하지 않습니다.")
         } else if deleteNumber == 0 {
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("취소되었습니다.")
         } else {
             basketList.remove(at: deleteNumber!-1)
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("\(deleteNumber!)번 상품이 삭제되었습니다.")
         }
     }
     
     //장바구니의 모든 상품을 삭제하는 메소드
     override func removeEverything() {
-        print("========================================")
+        print(String(repeating: "=", count: 30))
         print("장바구니의 상품을 전체삭제 하시겠습니까?")
         print("1. Yes \n2. No")
         let chooseOpetion = Int(readLine() ?? "2")
         if chooseOpetion == 1 {
             basketList.removeAll()
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("장바구니의 상품이 전체 삭제되었습니다.")
         } else {
-            print("========================================")
+            print(String(repeating: "=", count: 30))
             print("취소되었습니다.")
         }
     }
